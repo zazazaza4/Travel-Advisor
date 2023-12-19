daf7737c0e488f0b9f4318f94cdd140d55ea0716
Yaroslav <6715414@gmail.com>
2023-12-19 23:51:26 +0200

    feat: add assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as UserAvatar } from "./icon.jpg";
+export { default as OsakaCastle } from "./osaka-castle.jpg";
+export { default as TokyoTemple } from "./tokyo-temple.jpg";
+export { default as MammothLake } from "./mammoth-lake.jpg";
